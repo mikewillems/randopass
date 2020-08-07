f9f4e67c8a742454978dd3509eebfb0bdb620794
Mike Willems <mikewillems@users.noreply.github.com>
2020-08-06 17:41:31 -0700

    modified setup file so that project description will publish in pypi

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
-    readme = f.read()
+# with open('README.md') as f:
+#     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+# with open('LICENSE') as f:
+    # license = f.read()
 
 setup(
     name='randopass',
-    version='0.1.0',
+    version='0.0.3',
     description='Generate easy but secure passphrases',
     long_description=readme,
     author='Mike Willems',
     author_email='themikewillems@gmail.com',
     url='https://github.com/mikewillems/randopass',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests', 'docs'))
 )
