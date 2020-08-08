7ae7bb4e5b6e19eb4d59b156f342fd1486dab960
Mike Willems <mikewillems@users.noreply.github.com>
2020-08-07 19:39:31 -0700

    added command line script via setup.py, fixed README, added classifiers

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 
-# with open('README.md') as f:
-#     readme = f.read()
+with open('README.md') as f:
+    readme = f.read()
 
-# with open('LICENSE') as f:
-    # license = f.read()
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
     name='randopass',
@@ -18,5 +18,13 @@ setup(
     author_email='themikewillems@gmail.com',
     url='https://github.com/mikewillems/randopass',
     license='MIT',
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': ['randopass=command_line:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
